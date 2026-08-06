2d38745caf6ff967ac2eb649e4a305e05a08de4d
name-web <fanta.doumouya@epitech.eu>
2026-08-06 22:45:10 +0000

    chore(main): install swagger documentation dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.setGlobalPrefix('api');
+
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Kovo Backend API')
+    .setDescription('Documentation officielle de l\'API pour le test technique Kovo')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
