382780580a66ba843db410e02e2f269d31b892ef
name-web <fanta.doumouya@epitech.eu>
2026-08-06 22:51:11 +0000

    chore(main): listen on all network interfaces via 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -11,14 +11,20 @@ async function bootstrap() {
 
   const config = new DocumentBuilder()
     .setTitle('Kovo Backend API')
-    .setDescription('Documentation officielle de l\'API pour le test technique Kovo')
+    .setDescription(
+      "Documentation officielle de l'API pour le test technique Kovo",
+    )
     .setVersion('1.0')
     .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port, '0.0.0.0');
 }
+
 bootstrap();
