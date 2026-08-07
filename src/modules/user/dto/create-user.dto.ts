c212da1ca98dcd340174a509d692ed7ab13f78a7
name-web <fanta.doumouya@epitech.eu>
2026-08-07 10:45:32 +0000

    feat(user): add user repository layer

---
+++ src/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,22 @@
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email!: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(8)
+  password!: string;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+}
---
+++ src/modules/user/repositories/user.repository.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@/../../generated/prisma';
+import { PrismaService } from '@/../../src/database/prisma.service';
+import { IUserRepository } from '../interfaces/user-repository.interface';
+import { CreateUserDto } from '../dto/create-user.dto';
+
+@Injectable()
+export class UserRepository implements IUserRepository {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: CreateUserDto): Promise<User> {
+    return this.prisma.user.create({
+      data: {
+        email: data.email,
+        password: data.password,
+        name: data.name,
+      },
+    });
+  }
+
+  async findByEmail(email: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return this.prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+}
