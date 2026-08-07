f098b7ccc5678e2055aa1cdff49881eb3d8683d5
name-web <fanta.doumouya@epitech.eu>
2026-08-07 13:10:34 +0000

    feat(config): add robust env configuration module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { UserModule } from './modules/user/user.module';
-import { AuthModule } from './modules/auth/auth.module';
-import { PrismaModule } from './database/prisma.module';
+
+import appConfig from './config/app.config';
+import databaseConfig from './config/database.config';
+import jwtConfig from './config/jwt.config';
+import { envValidationSchema } from './config/validation.config';
 
 @Module({
-  imports: 
-  [
-  UserModule, 
-  AuthModule,
-  PrismaModule,
-],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [
+        appConfig,
+        databaseConfig,
+        jwtConfig,
+      ],
+      validationSchema: envValidationSchema,
+      validationOptions: {
+        allowUnknown: true,
+        abortEarly: true,
+      },
+    }),
+
+    UserModule,
+  ],
 })
 export class AppModule {}
---
+++ src/config/validation.config.ts
@@ -0,0 +1,6 @@
+import * as Joi from 'joi';
+
+export const envValidationSchema = Joi.object({
+  DATABASE_URL: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
+});
