1feac40359b7bcd9925dccd83a257841d55613a1
name-web <fanta.doumouya@epitech.eu>
2026-08-07 10:58:39 +0000

    feat(user): implement service logic with bcrypt encryption

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { User } from '../../../generated/prisma';
+import { UserRepository } from './repositories/user.repository';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  async register(createUserDto: CreateUserDto): Promise<User> {
+    const existingUser = await this.userRepository.findByEmail(createUserDto.email);
+    
+    if (existingUser) {
+      throw new ConflictException('Cet email est déjà utilisé.');
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(createUserDto.password, salt);
+
+    return this.userRepository.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+  }
+
+  async getProfile(id: string): Promise<User> {
+    const user = await this.userRepository.findById(id);
+
+    if (!user) {
+      throw new NotFoundException('Utilisateur introuvable.');
+    }
+
+    return user;
+  }
+}
